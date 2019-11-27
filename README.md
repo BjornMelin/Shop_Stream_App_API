f8d9e6d2bd260e70df947f1cc038935aef338d2b
Bjorn Melin <melinb@augsburg.edu>
2019-11-26 22:02:30 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Shop_Stream_App_API
+ ShopStream Software Application Data API.  Uses Mongoose to connect to MongoDB.
---
+++ server.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const express = require('express');
+var cors = require('cors');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const { Customer } = require('./customer');
+
+const API_PORT = 4000;
+const app = express();
+app.use(cors());
+const router = express.Router();
+
+// this is our MongoDB database
+const dbRoute = 'mongodb://127.0.0.1:27017/AppData';
+
+// connects our back end code with the database
+mongoose.connect(dbRoute, { useNewUrlParser: true, useUnifiedTopology: true });
+
+let db = mongoose.connection;
+
+db.once('open', () => console.log('connected to the database'));
+
+// checks if connection with the database is successful
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// (optional) only made for logging and
+// bodyParser, parses the request body to be a readable json format
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+
+
+
+
+router.post('/putCustData', async (req, res) => {
+  // console.log(req);
+  console.log(req.body);
+  const {
+    nameFirst, nameLast, companyName, email, phoneNum,
+  } = req.body;
+  let newCust = new Customer({
+    nameFirst,
+    nameLast,
+    companyName,
+    email,
+    phoneNum,
+  });
+  const result = newCust.save();
+  res.send(result);
+});
+
+
+
+// append /api for our http requests
+app.use('/api', router);
+
+// launch our backend into a port
+app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
